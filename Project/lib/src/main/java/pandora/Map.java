902cf928a42af72c71af5bf2f469ceeffd76e2a0
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-07-10 20:01:57 +0200

    Update Map.java

--- Project/lib/src/main/java/pandora/Map.java
+++ Project/lib/src/main/java/pandora/Map.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -28,49 +29,42 @@ public class Map {
     private JFrame frame = new JFrame();
     private JPanel panel;
     Color color;
-    	
-    public Map()	{}
+    
 	public Map(int width, int height, int plantDensity) {
         this.width = width;
         this.height = height;
         this.density = plantDensity;
         this.map = new char[height][width];
         this.Trees_numb = 0;
-        this.Bushes_numb = 0;
-        
+        this.Bushes_numb = 0;    
         p_w=w/width;
         p_h=z/height;
     }
 	// metoda generująca mapę
     public void generate() {
         Random rand = new Random();
-        for (int row = 0; row < height; row++) {
-            for (int col = 0; col < width; col++) {
+        for (int row = 0; row < height; row++)
+        {
+            for (int col = 0; col < width; col++)
+            {
                 // losowe umieszczenie roślin na mapie
-                if (rand.nextInt(100) < density) {
-                    // losowy wybór typu rośliny
-                    if (rand.nextInt(5) == 0) {
-                        map[row][col] = 'T'; // T-drzewo
-                        Trees_numb++;
-                    } else {
-                        map[row][col] = 'B'; // B-krzak
-                        Bushes_numb++;
-                    }
-                } else {
-                    map[row][col] = '_'; // pole puste
+                if (rand.nextInt(100) < density) {		// losowy wybór typu rośliny
+                    if (rand.nextInt(5) == 0) 	{map[row][col] = 'T'; Trees_numb++;} // T-drzewo
+                    else 						{map[row][col] = 'B'; Bushes_numb++;} // B-krzak
                 }
+                else 	{map[row][col] = '_';} // pole puste
             }
         }
     }
     public void images()	//metoda do zaladowania plikow .png
     {
     	this.image[0]=getImage("bull.png");
-        this.image[1]=getImage("bush.png");
-        this.image[2]=getImage("tree.png");
-        this.image[3]=getImage("archer.png");
-        this.image[4]=getImage("rider.png");
-        this.image[5]=getImage("soldier.png");
-        this.image[6]=getImage("robot.png");
+    	this.image[1]=getImage("soldier.png");
+        this.image[2]=getImage("robot.png");
+        this.image[3]=getImage("rider.png");
+        this.image[4]=getImage("archer.png");
+        this.image[5]=getImage("bush.png");
+        this.image[6]=getImage("tree.png");
     }
     private Image getImage(String file)
     {
@@ -94,7 +88,7 @@ public class Map {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
-    // metoda wyświetlająca mapę na ekranie
+    // metoda wyswietlająca mapę na ekranie
     public void display(Graphics g)
     { 
     	color = new Color(74,190,100); g.setColor(color);	//kolor mapy
@@ -105,37 +99,26 @@ public class Map {
             	g.fillRect(col*p_w,row*p_h,p_w,p_h);	//wypelnienie danego pola mapy kolorem	
             	switch(map[row][col])
             	{
-            	case 'T': g.drawImage(image[2],col*p_w,row*p_h,p_w,p_h,null); break; //wyswietlenie tree.png jesli pole jest drzewem
-            	case 'B': g.drawImage(image[1],col*p_w,row*p_h,p_w,p_h,null); break; //wyswietlenie bush.png jesli pole jest krzakiem
+            	case 'T': g.drawImage(image[6],col*p_w,row*p_h,p_w,p_h,null); break; //wyswietlenie tree.png jesli pole jest drzewem
+            	case 'B': g.drawImage(image[5],col*p_w,row*p_h,p_w,p_h,null); break; //wyswietlenie bush.png jesli pole jest krzakiem
             	default: break;
             	}
             }
         }
     }
+    // metoda wyswietlejaca jednostki na wczesniej wyswietlonej mapie
     public void displayUnits(Graphics g,Interface[] navi,Interface[] col)
     {
-    	for(int index=0;index<navi.length;index++)
+    	ArrayList<Interface[]> list = new ArrayList<>();
+    	list.add(navi);
+    	list.add(col);
+    	for(Interface [] team : list)
     	{
-    		if(((Unit)navi[index]).health>0)	//unikniecie wyswietlania jednostek juz martwych
+    		for(Interface unit : team)
     		{
-    			switch(((Unit)navi[index]).type)
-    			{
-    			case 3: g.drawImage(image[4],((Unit)navi[index]).pos_x*p_w,((Unit)navi[index]).pos_y*p_h,p_w,p_h,null); break; //wyswietlenie rider.png na zajmowanej pozycji
-    			case 4: g.drawImage(image[3],((Unit)navi[index]).pos_x*p_w,((Unit)navi[index]).pos_y*p_h,p_w,p_h,null); break; //wyswietlenie archer.png na zajmowanej pozycji
-    			}
-    		}
-    	}
-    	for(int index=0;index<col.length;index++)
-    	{
-    		if(((Unit)col[index]).health>0)		//unikniecie wyswietlania jednostek juz martwych
-    		{
-    			switch(((Unit)col[index]).type)
-    			{
-    			case 0: g.drawImage(image[0],((Unit)col[index]).pos_x*p_w,((Unit)col[index]).pos_y*p_h,p_w,p_h,null); break; //wyswietlenie bull.png na zajmowanej pozycji
-    			case 1: g.drawImage(image[5],((Unit)col[index]).pos_x*p_w,((Unit)col[index]).pos_y*p_h,p_w,p_h,null); break; //wyswietlenie soldier.png na zajmowanej pozycji
-    			case 2: g.drawImage(image[6],((Unit)col[index]).pos_x*p_w,((Unit)col[index]).pos_y*p_h,p_w,p_h,null); break; //wyswietlenie robot.png na zajmowanej pozycji
-    			}
-    		}
+    			if(((Unit)unit).health>0) {	//unikniecie wyswietlania jednostek juz martwych
+    			g.drawImage(image[((Unit)unit).type],((Unit)unit).pos_x*p_w,((Unit)unit).pos_y*p_h,p_w,p_h,null);}
+    		}	//wyswietlenie pliku png danej jednostki (indeks odpowiedniego pliku w tablicy image jest identyczny wartosci "type" jednostki)
     	}
     }
     
